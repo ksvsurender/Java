089b629754ab40cf16a3ac3efa704d1c7d68d476
ksvsurender <131520430+ksvsurender@users.noreply.github.com>
2023-05-22 17:12:51 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,9 @@
+class Number
+{
+public static void main (String args[])
+{
+int a=Integer.parseInt(args[0]);
+if (a>0){System.out.println("Positive");}
+else if (a==0){System.out.println("Zero");}
+else{System.out.println("Negative");}
+}}
